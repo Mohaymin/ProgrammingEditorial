90dd22ec5cbd6253ae83b6af20bf42ffb7ff5e77
Ahmad Subaktagin Jabir <33084374+jabirsubaktagin16@users.noreply.github.com>
2019-04-08 07:23:36 +0600

    Add files via upload

---
+++ Programming Editorial New (8 April)/includes/footer.php
@@ -0,0 +1,23 @@
+<!-- Footer -->
+<footer style="background-color: #018786;">
+
+	<!-- Copyright -->
+	<div class="row">
+		<div class="col-md-4">
+			<p>AHMAD SUBAKTAGIN JABIR</p>
+			<p>CHOWDHURY ABDULLAH AL MOHAYMIN</p>
+		</div>
+			
+		<div class="col-md-4">
+			<p>16.02.04.061</p>
+			<p>16.02.04.075</p>
+		</div>
+	</div>
+	<div class="text-center py-3">
+		© 2019 Copyright:
+		<a href="#"> programmingeditorial.com </a>
+	</div>
+	<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
